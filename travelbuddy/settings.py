91cde34d63345fd3c3e49eb3be8fa71b68197748
Pitso <Pitsotladi71@gmail.com>
2026-09-01 12:04:25 +0200

    redid user model to use abstract user

--- travelbuddy/settings.py
+++ travelbuddy/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users',
+    'destinations',
 ]
 
+AUTH_USER_MODEL = 'users.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- users/models.py
+++ users/models.py
@@ -1,32 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
+
+class User(AbstractUser):
+
     name = models.CharField(max_length=100)
+
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100, )
-    prev_destinations = models.TextField(blank=True, null=True)
-    #ranks 1.rookie(suitcase)|2. voyager(compass)|3. explorer(map)|4.nomad(globe)
-    '''
-    | Action                       | Points |
-| ---------------------------- | -----: |
-| Write a review               |    +10 |
-| Review gets a helpful/like   |     +5 |
-| Add a photo                  |     +3 |
-| Review a new destination     |    +10 |
-| Review gets reported/removed |    −20 |
-| Receive a “helpful” vote     |     +5 |
- 1.rookie = 0-50 pts, 2.voyager = 51-100 pts, 3.explorer = 101-200 pts, 4.nomad = 201+ pts
-    
-    '''
 
+    bio = models.TextField(
+        max_length=120,
+        blank=True,
+        null=True
+    )
 
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        blank=True,
+        null=True
+    )
 
-    review_rank = models.CharField(max_length=10)
-    bio = models.TextField(max_length=120, blank=True, null=True)
-    avartar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    budget = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00
+    )
 
+    review_points = models.PositiveIntegerField(
+        default=0
+    )
 
     def __str__(self):
         return self.name
