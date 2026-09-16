d0852f03bd611d41b6602b4405cd7c7c59cf6bc6
Pitso <Pitsotladi71@gmail.com>
2026-09-16 20:31:52 +0200

    tested and validated API

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 
@@ -21,7 +22,8 @@ class Event(models.Model):
     price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        default=0.00
+        default=0.00,
+        validators= [MinValueValidator(0)]
     )
 
     interest = models.ManyToManyField('interests.Interest')
